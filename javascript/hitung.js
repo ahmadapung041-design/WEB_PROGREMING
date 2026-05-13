17c7b8711aba848a00a84fd780054b414c0440c5
ahmadapung041-design <ahmadapung041@gmail.com>
2026-05-13 09:33:54 +0700

    Add files via upload

---
+++ javascript/hitung.js
@@ -0,0 +1,63 @@
+document.body.appendChild(createInput('x'))
+document.body.appendChild(createInput('y'))
+document.body.appendChild(buatKotak('hasil', 'green', '32px'))
+
+document.body.appendChild(buatTombol('tambah','+'))
+document.body.appendChild(buatTombol('kurang','-'))
+document.body.appendChild(buatTombol('kali','x'))
+document.body.appendChild(buatTombol('bagi','/'))
+document.body.appendChild(buatTombol('mod','%'))
+document.body.appendChild(buatTombol('hapushistory','Hapus History'))
+document.body.appendChild(buatTombol('hapusinput','Hapus Input'))
+
+document.body.appendChild(buatTombol('history','blue', '320px'))
+
+function createInput(id){
+    let input = document.createElement('input');
+    input.setAttribute('id', id);  
+    return input 
+}
+
+function buatTombol(id, text, warna){
+    let tbl = document.createElement('button');
+    tbl.setAttribute('id', id);
+    tbl.setAttribute('style','margin: 0 5px; border-radius; 5px; border : none; padding: 10px; cursor; pointer; background-color; #45919d; color; white; font-weight; bold;')
+    tbl.innerHTML = text;
+    return tbl
+}
+
+function buatKotak(id, warna, tinggi){
+    let kotak = document.createElement('div');
+    kotak.setAttribute('id', id);
+    kotak.setAttribute('style','overflow:auto; box-sizing; border-box 5px; border : none; padding: 10px; background-color; #45919d; color; white; font-weight; bold;')
+    kotak.innerHTML = text;
+    return kotak
+}
+
+function hitung(op, x, y){
+    switch(op){
+        case '+': return x + y;
+        case '-': return x - y;
+        case '*': return x * y;
+        case '/': return x / y;
+        case '%': return x % y;
+    }
+}
+
+let tambah = document.getElementById("tambah");
+tambah.addEventListener("click", function () {
+    let x = document.getElementById("x").value;
+    let y = document.getElementById("y").value;
+    if (x == "" || y == "") {
+        document.getElementById("hasil").innerHTML = "Inputan harus diisi";
+    } 
+    else if (isNaN(x) || isNaN(y)) {
+        document.getElementById("hasil").innerHTML = "Inputan harus berupa angka";
+    } 
+    else {
+        let hasil = hitung('+', Number(x), Number(y));
+        document.getElementById("hasil").innerHTML = hasil;
+        document.getElementById("history").innerHTML += 
+            `<p>${x} + ${y} = ${hasil}</p>`;
+    }
+});
